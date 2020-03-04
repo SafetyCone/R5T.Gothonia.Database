37f16093fcaa564669956802701886145cd11b46
MinexAutomation <minex.automation@gmail.com>
2020-03-03 23:38:58 -0800

    Changed to return the ModelBuilder instance to allow a fluent-API.

--- source/R5T.Gothonia.Database/Code/Extensions/ModelBuilderExtensions.cs
+++ source/R5T.Gothonia.Database/Code/Extensions/ModelBuilderExtensions.cs
@@ -7,11 +7,13 @@ namespace R5T.Gothonia.Database
 {
     public static class ModelBuilderExtensions
     {
-        public static void ForTextItemDbContext(this ModelBuilder modelBuilder)
+        public static ModelBuilder ForTextItemDbContext(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Entities.TextItem>().HasAlternateKey(x => x.GUID);
             modelBuilder.Entity<Entities.TextItemType>().HasAlternateKey(x => x.GUID);
             modelBuilder.Entity<Entities.TextItemType>().HasAlternateKey(x => x.Name);
+
+            return modelBuilder;
         }
     }
 }
